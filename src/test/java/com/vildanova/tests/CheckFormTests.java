076db4fffe69bb04a59f00505f15a1cc07fd6ac0
Kamilya Vildanova <vildanova-kamilya99@yandex.ru>
2022-08-11 23:12:01 +0300

    added allureListener

--- src/test/java/com/vildanova/tests/CheckFormTests.java
+++ src/test/java/com/vildanova/tests/CheckFormTests.java
@@ -1,6 +1,9 @@
 package com.vildanova.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -14,6 +17,7 @@ public class CheckFormTests {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browser = "FIREFOX";
         Configuration.startMaximized = true;
     }
