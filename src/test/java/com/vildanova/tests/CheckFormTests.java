0c207370e8370b241ac77e80f6c2aea162e5d239
Kamilya Vildanova <vildanova-kamilya99@yandex.ru>
2022-09-13 16:36:41 +0300

    added docker

--- src/test/java/com/vildanova/tests/CheckFormTests.java
+++ src/test/java/com/vildanova/tests/CheckFormTests.java
@@ -1,47 +1,15 @@
 package com.vildanova.tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import com.vildanova.helpers.Attach;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class CheckFormTests {
-
-    @BeforeAll
-    static void beforeAll() {
-
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-//        Configuration.browser="FIREFOX";
-        Configuration.startMaximized = true;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-    }
-
-    @AfterEach
-    public void tearDown() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
+public class CheckFormTests extends TestBase{
 
     @Test
     void fillFormTest() {
-
         open("/automation-practice-form");
 
         $("#firstName").setValue("Kamilya");
@@ -57,15 +25,5 @@ public class CheckFormTests {
         $("#hobbiesWrapper").$(byText("Music")).click();
         $("#uploadPicture").uploadFromClasspath("img/photo.png");
         $("#currentAddress").setValue("Ulyanovsk");
-//        $(By.xpath("//div[@class=' css-yk16xz-control'][1]")).click();
-//        $("#stateCity-wrapper").$(byText("NCR")).click();
-//        $("#city").click();
-//        $("#stateCity-wrapper").$(byText("Delhi")).click();
-//        $("#submit").click();
-//
-//        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
-//        $(".table-responsive").shouldHave(text("Kamilya Vildanova"), text("vildanova-kamilya99@yandex.ru"),
-//                text("Female"), text("9655976283"), text("17 November,1999"), text("Maths"), text("Music"),
-//                text("photo.png"), text("Ulyanovsk"));
     }
 }
