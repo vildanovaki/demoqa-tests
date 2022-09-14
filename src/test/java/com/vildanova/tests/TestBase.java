3ed1a17fca3cd23fcf9e5be16e741e9631764b91
Kamilya Vildanova <vildanova-kamilya99@yandex.ru>
2022-09-14 20:02:34 +0300

    added TestBase.java

---
+++ src/test/java/com/vildanova/tests/TestBase.java
@@ -0,0 +1,35 @@
+package com.vildanova.tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import com.vildanova.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.remote = "http://localhost:8080/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.startMaximized = true;
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+}
